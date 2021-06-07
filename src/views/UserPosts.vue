2477f8b13a234df73b6431a2bf6ed4aa4f6154de
DominykasDriu <dominykas.driukas@gmail.com>
2021-06-07 18:44:32 +0300

    add userPost logic

--- src/views/UserPosts.vue
+++ src/views/UserPosts.vue
@@ -9,6 +9,8 @@
           <h5 class="card-title">{{post.title}}</h5>
           <p class="card-text">{{truncateDescription(post.description)}}</p>
           <a class="btn btn-primary" @click="goToPost(post.username, post.id)">Read more</a>
+          <a class="btn btn-secondary ml-2" v-if="loggedUser.name === post.username" @click="goEditPost(post.username, post.id)">Edit</a>
+          <a class="btn btn-warning ml-2" v-if="loggedUser.name === post.username" @click="deletePost(post.id)">Delete</a>
         </div>
       </div>
     </main>
@@ -26,12 +28,33 @@ export default {
   data() {
     return {
       posts: null,
-      user: this.$route.params.name
+      user: this.$route.params.name,
+      loggedUser: JSON.parse(localStorage.getItem('blogUser'))
     }
   },
   methods: {
     goToPost(name, id) {
       this.$router.push(`/post/${name}/${id}`)
+    },
+    goEditPost(name,id) {
+      this.$router.push(`/edit/${name}/${id}`)
+    },
+    deletePost(id) {
+      fetch('http://167.99.138.67:1111/deletepost', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          secretKey: this.user.key,
+          id: id
+        })
+      })
+      .then(res => {
+        console.log(res);
+        this.$forceUpdate()
+      })
+      .catch(e => console.log(e))
     },
     truncateDescription(val) {
       return (val.length > 150) ? val.substr(0, 150-1) + ' ...' : val;
