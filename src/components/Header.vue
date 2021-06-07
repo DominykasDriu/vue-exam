260a374cc38cf03c00570581d19f4cbe76559f56
DominykasDriu <dominykas.driukas@gmail.com>
2021-06-07 18:24:22 +0300

    add logic to reach userPosts

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,8 +5,8 @@
       <router-link v-if="!loggedIn" to="/register">Register</router-link>
       <router-link v-if="!loggedIn" to="/login">Login</router-link>
       <router-link v-if="loggedIn" to="/create">Create new post</router-link>
-      <input type="text" placeholder="enter name of user" />
-      <button>Check Users Posts</button>
+      <input type="text" placeholder="enter name of user" v-model="userInput"/>
+      <button @click="goToUserPosts">Check Users Posts</button>
     </nav>
   </header>
 </template>
@@ -16,7 +16,15 @@ export default {
   name: 'Header',
   data() {
     return {
-      loggedIn: false
+      loggedIn: false,
+      userInput: null
+    }
+  },
+  methods: {
+    goToUserPosts() {
+      if (this.userInput) {
+        this.$router.push(`/user_posts/${this.userInput}`)
+      }
     }
   },
   mounted() {
